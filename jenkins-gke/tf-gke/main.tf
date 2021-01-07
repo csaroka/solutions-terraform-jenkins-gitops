d8b34d658b0cbff12d2201c7dec386888dfb37e4
csaroka <csaroka@gmail.com>
2021-01-07 11:30:49 -0500

    Version updates

--- jenkins-gke/tf-gke/main.tf
+++ jenkins-gke/tf-gke/main.tf
@@ -188,9 +188,8 @@ data "local_file" "helm_chart_values" {
 
 resource "helm_release" "jenkins" {
   name       = "jenkins"
-  repository = "https://charts.helm.sh/stable"
+  repository = "https://charts.jenkins.io"
   chart      = "jenkins"
-  version    = "2.5.4"
   timeout    = 1200
 
   values = [data.local_file.helm_chart_values.content]
